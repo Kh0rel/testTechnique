478f717901ee89e6cce70f2022cd4c3b10633274
CHIEB BOUARES <guillaume@chiebbouares.me>
2021-03-10 16:21:09 +0100

    Create UI Methods logic

--- TestTechnique/Modules/Offers/OffersProtocols.swift
+++ TestTechnique/Modules/Offers/OffersProtocols.swift
@@ -8,15 +8,31 @@
 import UIKit
 
 protocol OffersWireframeProtocol: class {
-   func presentDetail(with item: String)
+   func presentDetail(with item: Offer)
+   func presentSearch(with items: [Offer], title: String)
+   func dismiss()
 }
 
 protocol OffersViewProtocol: class {
+   func displayCloseButton()
+   func reloadData()
 }
 
 protocol OffersPresenterProtocol: class {
+   var sections: [Section<OfferListItem>] { get }
+   
+   func viewDidLoad() 
    func didSelectItem(at indexPath: IndexPath)
+   func didSelect(category: Category)
+   func didTapClosedButton()
 }
 
 protocol OffersInteractorProtocol: class {
+   func retrieveCategories(completionHandler: @escaping (Result<[Category], Error>) -> Void)
+   func retrieveOffers(completionHandler: @escaping (Result<[Offer], Error>) -> Void)
+}
+
+enum OfferListItem: FormattedModelProtocol {
+   case categories([Category])
+   case offer(Offer)
 }
