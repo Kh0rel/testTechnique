d04ba8f1e2f8fd378a76a6ee0ac0c59b8210803d
CHIEB BOUARES <guillaume@chiebbouares.me>
2021-03-10 16:20:36 +0100

    Create multiple DataManager and add it to interactor

--- TestTechnique/Modules/Offers/OffersInteractor.swift
+++ TestTechnique/Modules/Offers/OffersInteractor.swift
@@ -8,9 +8,30 @@
 import Foundation
 
 final class OffersInteractor {
+
 }
 
 // MARK: - Extensions -
 
 extension OffersInteractor: OffersInteractorProtocol {
+   
+   func retrieveOffers(completionHandler: @escaping (Result<[Offer], Error>) -> Void) {
+      let builderResult = HttpRequestBuilder().buildRequest(with: .offers).extractUrlRequest()
+      do {
+         let offerDataFetcher = OfferDataFetcher(session: URLSession.shared)
+         offerDataFetcher.fetchOffers(from: try builderResult.get(),completionHandler: completionHandler)
+      } catch {
+         print(error)
+      }
+   }
+   
+   func retrieveCategories(completionHandler: @escaping (Result<[Category], Error>) -> Void) {
+      let builderResult = HttpRequestBuilder().buildRequest(with: .categories).extractUrlRequest()
+      do {
+         let categoryDataFetcher = CategoryDataFetcher(session: URLSession.shared)
+         categoryDataFetcher.fetchCategories(from: try builderResult.get(), completionHandler: completionHandler)
+      } catch {
+         print(error)
+      }
+   }
 }
